b49f70661d2db04b353e8a1a6ed6fc0a3513402e
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-03-05 15:38:40 -0500

    cargo.mk: @echo cargo ...

--- cargo.mk
+++ cargo.mk
@@ -1,21 +1,31 @@
 cargo-build:## 	cargo-build
 	@. $(HOME)/.cargo/env
+	@echo cargo b
 	@cargo b
 cargo-install:## 	cargo-install
 	@. $(HOME)/.cargo/env
+	@echo cargo install --path $(PWD)
 	@cargo install --path $(PWD)
 	@echo "export PATH=$(CARGO_PATH)/bin:$(PATH)"
 cargo-build-release:## 	cargo-build-release
 	@. $(HOME)/.cargo/env
+	@echo cargo b --release
 	@cargo b --release
 cargo-check:## 	cargo-check
 	@. $(HOME)/.cargo/env
+	@echo cargo c
 	@cargo c
 cargo-bench:## 	cargo-bench
 	@. $(HOME)/.cargo/env
+	@echo cargo bench
 	@cargo bench
+cargo-clean:## 	cargo-clean
+	@. $(HOME)/.cargo/env
+	@echo cargo clean
+	@cargo clean
 cargo-test:## 	cargo-test
 	@. $(HOME)/.cargo/env
+	@echo cargo test
 	@cargo test
 # vim: set noexpandtab:
 # vim: set setfiletype make
